f74270a4a2355a5a07e1b4e80cb6c4e3d43969ce
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-05 15:59:21 +0900

    일기작성 화면 UI 구성

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 정한결 on 2023/05/05.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
