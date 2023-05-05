efe4d0ce00f68d5b88d81be9732ed8825b718fbb
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-05 16:07:20 +0900

    일기장 상세화면 구현

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 정한결 on 2023/05/05.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
